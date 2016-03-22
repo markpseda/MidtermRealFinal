2c9e56afbc2e359a9540efadfc1d0c5ec2e16fcf
Mark Seda <markpseda@gmail.com>
2016-03-21 20:16:47 -0400

    final

--- FinalMidterm/src/main/java/com/cisc181/core/Course.java
+++ FinalMidterm/src/main/java/com/cisc181/core/Course.java
@@ -9,24 +9,51 @@ public class Course {
 	private String CourseName;
 	private int GradePoints;
 	private eMajor Major;
-	public Course(String courseName, int gradePoints, eMajor major) {
-		super();
+	/**
+	 * No arg constructor. Generates new course with unique UUID
+	 */
+	public Course(){
 		this.CourseID = UUID.randomUUID();
+	}
+	/**
+	 * Course constructor with 3 parameters for course name, number  of grade points, and major
+	 * @param courseName
+	 * @param gradePoints
+	 * @param major
+	 */
+	public Course(String courseName, int gradePoints, eMajor major) {
+		this();
 		CourseName = courseName;
 		GradePoints = gradePoints;
 		Major = major;
 	}
+	
 	public UUID getCourseID() {
 		return CourseID;
 	}
+	
 	public String getCourseName() {
 		return CourseName;
 	}
+	
 	public int getGradePoints() {
 		return GradePoints;
 	}
+	
 	public eMajor getMajor() {
 		return Major;
+	}
+	
+	public void setCourseName(String courseName) {
+		CourseName = courseName;
+	}
+	
+	public void setGradePoints(int gradePoints) {
+		GradePoints = gradePoints;
+	}
+	
+	public void setMajor(eMajor major) {
+		Major = major;
 	}
 	
 	
